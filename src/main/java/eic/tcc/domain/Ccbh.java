595ed5114242b9faf21c3eb9fd7cc35e8cf1bb61
Rafael Santos da Silva <rafaels@outlook.com.br>
2022-02-19 02:41:51 -0300

    Criação da entidade Ccbh

---
+++ src/main/java/eic/tcc/domain/Ccbh.java
@@ -0,0 +1,24 @@
+package eic.tcc.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_ccbh")
+public class Ccbh {
+
+	@Id
+	@Column(name = "ccbh_id")
+	private String id;
+
+	@Column(name = "seq_name")
+	private String seqName;
+
+	@Column
+	private String description;
+
+	@Column
+	private Integer length;
+}
